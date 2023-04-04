122538abe076b587b21411314cb8ebe7ae823bb3
ANDRnewHuman <blsckcollapse@gmail.com>
2023-04-04 22:23:53 +0300

    1

---
+++ scripts.js
@@ -0,0 +1,31 @@
+$("#slider").slick({
+  slidesToShow: 4,
+  appendArrows: $(".div-button"),
+  prevArrow:
+    '<button class="slider-arrow arrow-prev">"<svg width="25" height="25"><use href="./image/sprite.svg#icon-right"></use> </svg></button>',
+  nextArrow:
+    '<button class="slider-arrow arrow-prev">"<svg width="25" height="25"><use href="./image/sprite.svg#icon-left"></use> </svg></button>',
+});
+$("#slider-two").slick({
+  slidesToShow: 5,
+  prevArrow:
+    '<button class="slider-compainer ">"<svg width="20" height="20"><use href="./image/sprite.svg#icon-right"></use> </svg></button>',
+  nextArrow:
+    '<button class="slider-compainer-two">"<svg width="20" height="20"><use href="./image/sprite.svg#icon-left"></use> </svg></button>',
+});
+$("#slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: $("#slider-nav"),
+});
+$("#slider-nav").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  dots: false,
+
+  asNavFor: $("#slider-for"),
+  centerMode: false,
+  focusOnSelect: false,
+});
